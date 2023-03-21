d443ee5e44deaad99d7b522d33eddabcf57cd15c
Kuniyoshi Koichiro <kuniyoshi.koichiro.345611@gmail.com>
2023-03-21 20:49:35 +0900

    fix my function

--- 07_ListsAndRecursion/4.exs
+++ 07_ListsAndRecursion/4.exs
@@ -1,5 +1,13 @@
 defmodule MyList do
   def span(from, to) do
-    Enum.to_list(from..to)
+    to_list(from..to)
+  end
+
+  def to_list(from..to) when from == to do
+    [from]
+  end
+
+  def to_list(from..to) do
+  [from | to_list((from + 1)..to)]
   end
 end
