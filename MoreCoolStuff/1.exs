e5d61b44c06b10e112e494d17355459cd938e1f1
kuniyoshi9244 <kuniyoshi.koichiro.345611@gmail.com>
2023-04-20 11:46:03 +0900

    Add MoreCoolStuff-1

---
+++ MoreCoolStuff/1.exs
@@ -0,0 +1,22 @@
+defmodule CsvSigil do
+  def sigil_v(lines, _opts) do
+    lines
+    |> String.trim_trailing()
+    |> String.split()
+    |> Enum.map(fn line -> String.split(line, ",") end)
+  end
+end
+
+defmodule Example do
+  
+  import CsvSigil
+
+  def create_csv do
+    ~v"""
+    1,2,3
+    cat,dog
+    """
+  end
+end
+
+IO.inspect Example.create_csv()
